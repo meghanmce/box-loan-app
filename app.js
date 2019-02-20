d706461216a9590946f0dc69aeb39cac3503b9a6
Meghan McEneaney <mmceneaney@ri.pn>
2019-02-20 18:41:43 -0500

    fix hardcoded file/ folder values

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const BoxSDK = require('box-node-sdk');  // Box SDK
 const express = require('express');	
 const app = express();
 const exphbs = require('express-handlebars');
-// const path = require('path');
 const fs = require('fs');
-const session = require('express-session')
 const bodyParser = require('body-parser');  // Exposes submitted form values on req.body in route handlers
 
 var sdkConfig = require('./config.json');
 var sdk = BoxSDK.getPreconfiguredInstance(sdkConfig);
+const formidableMiddleware = require('express-formidable');
 
 app.use(express.static('public')) // read in static css file
+
+app.use(formidableMiddleware());
 
 // Get the service account client, used to create and manage app user accounts
 var client = sdk.getAppAuthClient('enterprise');
@@ -27,24 +28,17 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-// Set up sessions 
-app.use(session({
-	secret: 'session secret',
-	resave: false,
-	saveUninitialized: false
-}));
-
 app.get('/', (req,res) => res.render('upload'))
 
 app.get('/success', (req,res) => res.render('success'))
 
 //	Create a new folder with their user name and place the file in their folder
 app.post('/', (req,res) => {	
-	client.folders.create('0', 'New Client')
+	client.folders.create('0', req.fields['name'])
 		.then(folder => {			
 			console.log(folder.id)
-			var stream = fs.createReadStream('/Users/meghanmceneaney/Desktop/MeghanResume.pdf');
-			client.files.uploadFile(folder.id, 'clientapp.pdf', stream) 
+			var stream = fs.createReadStream(req.files.file.path);
+			client.files.uploadFile(folder.id, req.files.file.name , stream) 
 		});
 	res.redirect('/success')
 });
